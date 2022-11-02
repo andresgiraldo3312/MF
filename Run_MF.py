9543893603b8d8832f761b624352d91f47ea8ab6
Andres Felipe Giraldo Forero <afgiraldofo@gmail.com>
2022-11-01 21:47:57 -0500

    the code was tested

--- Run_MF.py
+++ Run_MF.py
@@ -5,11 +5,11 @@ import MF
 
 for i in range(6):
     
-    df = pd.read_csv('DatosEliminados/Ventana_Eli_mes'+ str(i+1) +'.csv')
+    df = pd.read_csv('DatosEliminados/Ventana_Eli_mes'+ str(i+1) +'.csv', sep=',', index_col=0)
     Matriz_Eva = np.asarray(df)
     MetodoMF = MF.MF(R = Matriz_Eva, K = 360, beta = 0.1, iterations = 100) 
     MetodoMF.train()
     ME = MetodoMF.full_matrix()
     completado = pd.DataFrame(ME)
-    completado.to_csv('DatosEstimados/Ventana_Estimados_mes' + str(i+1) + '.csv', index = False)
+    completado.to_csv('DatosEstimados/Ventana_Estimada_mes' + str(i+1) + '.csv', index = False)
 
